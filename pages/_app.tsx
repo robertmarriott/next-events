0e8067eaee752b9fcca40bea947c556ab76fe62a
Robert Marriott <marriott@telus.net>
2021-03-19 21:52:52 -0700

    Convert to TypeScript

---
+++ pages/_app.tsx
@@ -0,0 +1,9 @@
+import 'semantic-ui-css/semantic.min.css';
+
+import type { AppProps } from 'next/app';
+
+const MyApp = ({ Component, pageProps }: AppProps) => {
+  return <Component {...pageProps} />;
+};
+
+export default MyApp;
