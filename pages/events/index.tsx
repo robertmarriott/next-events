f5b07300d2e34e246f150d74c72faa820ae2e37d
Robert Marriott <marriott@telus.net>
2021-03-25 21:03:42 -0700

    Create prototype of events page

--- pages/events/index.tsx
+++ pages/events/index.tsx
@@ -1,16 +1,30 @@
 import { useEffect, useState } from 'react';
+import { Container, Grid } from 'semantic-ui-react';
 
 import { getEvents } from '../../data/data';
 import Event from '../../types/Event';
 
-const EventsPage = () => {
+const Events = () => {
   const [events, setEvents] = useState<Event[]>([]);
 
   useEffect(() => {
     setEvents(getEvents());
-  }, [getEvents]);
+  }, []);
 
-  return <div></div>;
+  return (
+    <>
+      <Container>
+        <h1>Events</h1>
+        {events.map(({ id, title }) => (
+          <Grid.Row key={id}>
+            <Grid.Column>
+              <p>{title}</p>
+            </Grid.Column>
+          </Grid.Row>
+        ))}
+      </Container>
+    </>
+  );
 };
 
-export default EventsPage;
+export default Events;
